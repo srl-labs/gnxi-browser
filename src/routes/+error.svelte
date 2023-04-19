133806a1f55cb7a565d3d21b5aaf854021f604bc
Siva Sivakumar <siva.sivakumar@nokia.com>
2023-04-19 13:23:19 +0200

    draft 1.2

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,16 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 
 	import { page } from '$app/stores';
 </script>
 
-<div class="section">
-	<div class="container">
-		<article class="message is-danger">
-			<div class="message-header">
+<div class="p-7 font-nunito">
+	<div class="grid place-items-center content-center">
+		<article class="rounded min-w-[400px]">
+			<div class="text-white bg-red-400 px-5 py-3 rounded-t">
 				<p>Error {$page.status}</p>
 			</div>
-			<div class="message-body">{$page.error.message}</div>
+			<div class="bg-red-50 p-5 rounded-b text-red-500">{$page.error.message}</div>
 		</article>
 	</div>
 </div>
