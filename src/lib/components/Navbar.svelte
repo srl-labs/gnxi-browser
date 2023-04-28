076807b2e3b9933bcb3db9d007299781574aeaed
Siva Sivakumar <siva.sivakumar@nokia.com>
2023-04-28 23:36:14 +0530

    draft 4.1

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,9 +3,9 @@
 
   // TOGGLE BETWEEN LIGHT AND DARK MODE
   const toggleDarkMode = () => {
-    document.documentElement.classList.toggle('dark');
-    document.getElementById("toggle-light-icon").classList.toggle('hidden');
-    document.getElementById("toggle-dark-icon").classList.toggle('hidden');
+    document.documentElement.classList.toggle("dark");
+    document.getElementById("toggle-dark-icon").classList.toggle("hidden");
+    document.getElementById("toggle-light-icon").classList.toggle("hidden");
   }
 
   const toggleSidebar = () => {
@@ -46,10 +46,10 @@
         </svg>
       </button>
       <button class="flex px-2" type="button" on:click={toggleDarkMode}>
-        <svg id="toggle-light-icon" class="w-5 h-5" fill="currentColor" stroke-width="1.5" stroke="currentColor" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+        <svg id="toggle-light-icon" class="w-5 h-5 text-gray-200 hidden" fill="currentColor" stroke-width="1.5" stroke="currentColor" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
         </svg>
-        <svg id="toggle-dark-icon" class="w-5 h-5 text-gray-200 hidden" fill="currentColor" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+        <svg id="toggle-dark-icon" class="w-5 h-5 text-gray-800" fill="currentColor" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>
         </svg>
       </button>
